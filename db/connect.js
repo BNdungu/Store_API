3f14f86ff98d6a6ef855d8d375e4711436d947e6
BNdungu <bryannganga909@gmail.com>
2023-08-13 23:03:35 +0300

    connectdb

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,3 @@
-// const mongoose = require('mongoose')
-
-// const connectDB = (url) => {
-//   return mongoose.connect(url, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log(`\nSERVER IS LISTENNING AT PORT: ${port}\n`))
-//     .catch((err) => console.log('\nSERVER COULDN\'T CONNECT TO THE DATABASE\n'))
-// }
-
-// module.exports = connectDB
-
 const mongo = require('mongoose')
 
 const connectDB = ((url) => {
